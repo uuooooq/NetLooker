a6810da9bfceb4ee652dce838650ae9fc317ea34
Wenter.Zhu <Wenter.Zhu@connext.com.cn>
2018-03-12 10:44:15 +0800

    Initial Commit

---
+++ NetLooker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NetLooker
+//
+//  Created by Wenter.Zhu on 2018/3/12.
+//  Copyright © 2018年 Wenter.Zhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
